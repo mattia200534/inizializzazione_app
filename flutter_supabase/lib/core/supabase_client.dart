d0e5e2620e968aac43837cfd7e6f952eca38dae1
Verri Mattia <mattiaverri05@gmail.com>
2025-12-09 12:22:02 +0100

    completato

---
+++ flutter_supabase/lib/core/supabase_client.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class SupabaseClientManager {
+  static Future<void> init() async {
+    await dotenv.load();
+    await Supabase.initialize(
+      url: dotenv.env['SUPABASE_URL']!,
+      anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
+    );
+  }
+
+  static SupabaseClient get client => Supabase.instance.client;
+}
